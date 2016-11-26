0f502a45789f7effdc89f810d1f0db8f449b1995
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2016-11-26 14:26:33 -0600

    Add Retrofit instance in the main activity

--- app/src/main/java/me/juancrg90/postalcodes/MainActivity.java
+++ app/src/main/java/me/juancrg90/postalcodes/MainActivity.java
@@ -1,13 +1,72 @@
 package me.juancrg90.postalcodes;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import me.juancrg90.postalcodes.api.GeoNamesClient;
+import me.juancrg90.postalcodes.api.GeoNamesSevice;
+import me.juancrg90.postalcodes.api.PostalCodes;
+import me.juancrg90.postalcodes.entities.PostalCode;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.editPostalCode)
+    EditText editPostalCode;
+    @Bind(R.id.buttonSubmit)
+    Button buttonSubmit;
+
+    GeoNamesClient geoNamesClient = new GeoNamesClient();
+    GeoNamesSevice geoNamesSevice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        geoNamesSevice = geoNamesClient.getGeoNamesService();
     }
+
+    @OnClick(R.id.buttonSubmit)
+    public void handleSubmitClick() {
+        String country = "MX";
+        String maxRows = "2";
+        String postalcode = "37419";
+        String username = "JuanCrg90";
+        Call<PostalCodes> call = geoNamesSevice.listPostalCodes(country, maxRows, postalcode, username);
+
+        call.enqueue(new Callback<PostalCodes>() {
+            @Override
+            public void onResponse(Call<PostalCodes> call, Response<PostalCodes> response) {
+
+                List<PostalCode> postalCodeList = response.body().getPostalCodes();
+
+                for(int i = 0; i < postalCodeList.size(); i++) {
+                    Log.v("res", postalCodeList.get(i).getPlaceName());
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<PostalCodes> call, Throwable t) {
+                Toast.makeText(getApplicationContext(), "Something went wrong: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+    }
+
+
 }
