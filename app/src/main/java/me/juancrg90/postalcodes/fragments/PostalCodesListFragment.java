0deb6c0d3d5ef24aaefbdad6f4118e606a5c9cf4
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2016-11-26 16:12:35 -0600

    Add RecyclerView and Adapter Initialization in the Fragment

--- app/src/main/java/me/juancrg90/postalcodes/fragments/PostalCodesListFragment.java
+++ app/src/main/java/me/juancrg90/postalcodes/fragments/PostalCodesListFragment.java
@@ -3,18 +3,30 @@ package me.juancrg90.postalcodes.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import me.juancrg90.postalcodes.R;
+import me.juancrg90.postalcodes.adapters.PostalCodeAdapter;
+import me.juancrg90.postalcodes.entities.PostalCode;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PostalCodesListFragment extends Fragment implements PostalCodesListFragmentListener {
 
+    @Bind(R.id.recyclerView)
+    RecyclerView recyclerView;
+
+    PostalCodeAdapter adapter;
 
     public PostalCodesListFragment() {
         // Required empty public constructor
@@ -25,11 +37,43 @@ public class PostalCodesListFragment extends Fragment implements PostalCodesList
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_postal_codes_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_postal_codes_list, container, false);
+        ButterKnife.bind(this, view);
+        initAdapter();
+        initRecyclerView();
+        return view;
+    }
+
+
+    private void initAdapter() {
+        if(adapter == null) {
+            adapter = new PostalCodeAdapter(getActivity().getApplicationContext(), new ArrayList<PostalCode>());
+        }
+    }
+
+    private void initRecyclerView() {
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(adapter);
     }
 
     @Override
-    public void action(String message) {
-        Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
+    public void addToList(List<PostalCode> postalCodeList) {
+        clearList();
+
+        for(int i = 0; i < postalCodeList.size(); i++) {
+            adapter.add(postalCodeList.get(i));
+        }
+
+    }
+
+    @Override
+    public void clearList() {
+        adapter.clear();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
     }
 }
--- app/src/main/java/me/juancrg90/postalcodes/fragments/PostalCodesListFragmentListener.java
+++ app/src/main/java/me/juancrg90/postalcodes/fragments/PostalCodesListFragmentListener.java
@@ -1,9 +1,14 @@
 package me.juancrg90.postalcodes.fragments;
 
+import java.util.List;
+
+import me.juancrg90.postalcodes.entities.PostalCode;
+
 /**
  * Created by JuanCrg90 on 11/26/16.
  */
 
 public interface PostalCodesListFragmentListener {
-    public void action(String message);
+    void addToList( List<PostalCode> postalCodeList);
+    void clearList();
 }
